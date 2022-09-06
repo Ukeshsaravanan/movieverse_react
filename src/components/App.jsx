5894366ee5c98c2d73b1d3684cc4fa4c560a51d7
UKESH KUMAR <ukeshkumar@UKESHs-MacBook-Air.local>
2022-09-06 17:51:30 +0530

    Style sheet changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { css } from '@emotion/css';
+import { Route, Routes } from 'react-router-dom';
+// import { css } from '@emotion/css';
 import Movies from './Movies/Movies';
 import Actors from './Actors/Actors';
 import MovieInformation from './MovieInformation/MovieInformation';
 import Profies from './Profiles/Profies';
 import NavBar from './NavBar/NavBar';
+import { Content, Main, StyledToolbar } from './style';
 
 function App() {
   return (
-    <div className={css`display:flex;height:100%;`}>
+    <Main>
       <CssBaseline />
       <NavBar />
-      <main className={css`flex-grow:1;padding:2em;`}>
-        <div className={css`height:70px`}>
+      <Content>
+        <StyledToolbar>
+          <Routes>
+            <Route path="movie/:id" element={<MovieInformation />} />
+            <Route path="actors/:id" element={<Actors />} />
+            <Route path="/" element={<Movies />} />
+            <Route path="profile/:id" element={<Profies />} />
+          </Routes>
+        </StyledToolbar>
+      </Content>
 
-          <BrowserRouter>
-            <Routes>
-              <Route path="movie/:id" element={<MovieInformation />} />
-              <Route path="actors/:id" element={<Actors />} />
-              <Route path="/" element={<Movies />} />
-              <Route path="profile/:id" element={<Profies />} />
-            </Routes>
-          </BrowserRouter>
-
-        </div>
-      </main>
-
-    </div>
+    </Main>
   );
 }
 
---
+++ src/components/style.jsx
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material';
+
+export const Main = styled('div')(() => ({
+  display: 'flex',
+  height: '100%',
+  width: '100%',
+}));
+export const StyledToolbar = styled('div')(() => ({
+  height: '70px',
+}));
+export const Content = styled('main')(() => ({
+  flexGrow: 1,
+  padding: '2em',
+}));
