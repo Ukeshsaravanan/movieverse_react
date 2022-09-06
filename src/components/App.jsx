998d4a90fc53ad78017de1abcf244d255b1174e1
UKESH KUMAR <ukeshkumar@UKESHs-MacBook-Air.local>
2022-09-06 17:35:55 +0530

    File and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { css } from '@emotion/css';
+import Movies from './Movies/Movies';
+import Actors from './Actors/Actors';
+import MovieInformation from './MovieInformation/MovieInformation';
+import Profies from './Profiles/Profies';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
-    <div>App</div>
+    <div className={css`display:flex;height:100%;`}>
+      <CssBaseline />
+      <NavBar />
+      <main className={css`flex-grow:1;padding:2em;`}>
+        <div className={css`height:70px`}>
+
+          <BrowserRouter>
+            <Routes>
+              <Route path="movie/:id" element={<MovieInformation />} />
+              <Route path="actors/:id" element={<Actors />} />
+              <Route path="/" element={<Movies />} />
+              <Route path="profile/:id" element={<Profies />} />
+            </Routes>
+          </BrowserRouter>
+
+        </div>
+      </main>
+
+    </div>
   );
 }
 
---
+++ src/components/Profiles/Profies.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Profies() {
+  return (
+    <div>Profies</div>
+  );
+}
+
+export default Profies;
